6a28d0ab6d56efe7fb5a8baaaaebdd150d6960fe
Ravindra <sirviravindra609@gmail.com>
2024-08-27 20:50:40 +0530

    initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Market-bridge",
+  description:
+    "Market Bridge connects manufacturers with wholesalers, stremliining the supply chain and enabling efficient, direct transaction between production and distribution ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="items-center text-4xl font-black justify-center flex justify-flex mt-10">
+        Market-Bridge
+      </h1>
+      <p className="justify-center flex mt-10">
+        Market Bridge connects manufacturers with wholesalers, stremliining the
+        supply chain and enabling efficient, direct transaction between
+        production and distribution
+      </p>
+    </>
+  );
+}
